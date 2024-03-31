0dac4e8564acd34ec4dd42b6ddb9e2406deb7b47
pravesh dav <humantorch1932@gmail.com>
2024-03-31 16:32:52 +0530

    add section 3 with a parallex animation

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+function locoScroll() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main-wrapper"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  // tell ScrollTrigger to use these proxy methods for the "#main-wrapper" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy("#main-wrapper", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector("#main-wrapper").style.transform
+      ? "transform"
+      : "fixed",
+  });
+
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+}
+locoScroll();
 function handleHeaderHover() {
   gsap.to("header", {
     height: "40vh",
@@ -48,4 +88,31 @@ section2Cards.forEach((cards) => {
       scale: 0,
     });
   });
+});
+
+// parallax effect where image along with scrolling
+gsap.to("#section-3-img", {
+  y: 560,
+  duration: 3,
+  x: 770,
+  transform: "rotateY(180deg)",
+  scrollTrigger: {
+    trigger: "#section-3",
+    scroller: "#main-wrapper",
+    start: "top 11%",
+    end: "45% 10%",
+    scrub: 3,
+  },
+});
+gsap.to("#rotate-img", {
+  rotate: 360,
+  duration: 6,
+  scrollTrigger: {
+    trigger: "#section-4",
+    scroller: "#main-wrapper",
+    start: "top 91%",
+    end: "top 10%",
+    markers: true,
+    scrub: 10,
+  },
 });
